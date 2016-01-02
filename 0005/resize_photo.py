2be9387792797962b29b83a938ae5942acbf8918
Henry-Z <chancidy@gmail.com>
2016-01-02 00:00:01 -0500

    0005

--- 0005/resize_photo.py
+++ 0005/resize_photo.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
 
-def resize_pic(path, new_path, width = 640, height = 1136):
+def resize_pic(path, width = 640, height = 1136):
 	im = Image.open(path)
 	w,h = im.size
+	changew = float(w) / width
+	changeh = float(h) / height
+
+	if w > width or h > height:
+		change = changew if changew >changeh else changeh
+		print (change)
+		im.resize((int(w / change),int (h / change))).save('result.jpg')
+	else :
+		print ("the picture does not exceed iPhone5's resolution")
+
+if __name__ == '__main__':
+	resize_pic('image.png')
